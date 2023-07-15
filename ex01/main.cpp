b8c6de0b16a0d55b1b1a2c9fb484e213ff66209d
Hassan Lakhal <hlakhal-@e1r4p10.1337.ma>
2023-07-15 01:09:09 +0100

    fixed display search

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,27 +6,46 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 00:26:00 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/07/14 18:34:17 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/07/15 01:06:44 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
+
+void dispaly(std::string str)
+{
+	if(str.length() > 10)
+			std::cout << std::setw(9) << str.substr(0,9) << ".|";
+	else
+		std::cout << std::setw(10) << str << "|";
+}
+
+void	Contact::display_info(void)
+{
+	dispaly(this->first_name);
+	dispaly(this->last_name);
+	dispaly(this->nick_name);
+	std::cout << std::endl <<"---------------------------------------------" << std::endl;
+}
 
 void	PhoneBook::display()
 {
 	int i;
 
 	i = 0;
-	PhoneBook ob;
+	//PhoneBook ob;
 	std::cout << "---------------------------------------------" << std::endl;
 	std::cout << "|                 PhoneBook                 |" << std::endl;
 	std::cout << "---------------------------------------------" << std::endl;
 	std::cout << "|   Index  |First Name|Last Name | Nickname |" << std::endl;
 	std::cout << "---------------------------------------------" << std::endl;
-	// while(i < ob.id)
-	// {
-
-	// }
+	while(i <= this->id)
+	{
+		std::cout << "|" <<  std::setw(10)<< i << "|";
+		this->phone[i].display_info();
+		std::cout << std::endl;
+		i++;
+	}
 }
 
 void PhoneBook::add()
@@ -34,14 +53,18 @@ void PhoneBook::add()
 	static int i;
 	PhoneBook phone_ob;
 	this->phone[i % 8].init();
+	this->id = i;
+	if (this->id >= 8)
+		this->id = 0;
 	i++;
 }
+
 std::string Contact::rempler(std::string str)
 {
 	std::string number_str;
 	std::string input;
-	if(!strcmp(str.c_str(),"your number phone : "))
-		this->number = std::atoi(number_str.c_str());
+	// if(!strcmp(str.c_str(),"your number phone : "))
+	// 	this->number = std::atoi(number_str.c_str());
 	std::cout << str;
 	getline(std::cin, input);
 	return input;
@@ -50,8 +73,8 @@ std::string Contact::rempler(std::string str)
 void Contact::init(void)
 {
 	this->first_name = rempler("your first name : ");
-	this->last_name =  rempler("your last name : ");
-	this->nake_name = rempler("your Nickname : ");
+	this->last_name = rempler("your last name : ");
+	this->nick_name = rempler("your Nickname : ");
 	this->number_of_phone = rempler("your number phone : ");
 	this->darkest_secret = rempler("your darkest secret : ");
 }
@@ -59,15 +82,15 @@ void Contact::init(void)
 int	main(void)
 {
 	PhoneBook	contact;
-
 	std::string str;
-	contact.display();
 	while (1)
 	{
 		std::cout << "Enter command ";
 		getline(std::cin, str);
 		if(!strcmp(str.c_str(),"ADD"))
 			contact.add();
+		else if(!strcmp(str.c_str(),"SEARCH"))
+			contact.display();
 		else if(!strcmp(str.c_str(),"EXIT"))
 			exit(0);
 		//std::cout << "Hello " << str << "\n";
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,13 +6,14 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 00:25:57 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/07/14 17:59:14 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/07/14 22:36:05 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 #include<iostream>
+#include<iomanip>
 #include<string>
 
 class Contact
@@ -20,13 +21,14 @@ class Contact
 	private:
 		std::string first_name;
 		std::string last_name;
-		std::string nake_name;
+		std::string nick_name;
 		std::string darkest_secret;
 		std::string number_of_phone;
 		int number;
 	public:
 		void init();
 		void search();
+		void display_info();
 		std::string rempler(std::string str);
 };
 
