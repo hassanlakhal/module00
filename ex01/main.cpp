a9eaa34f0c3bd90eb35e9d07009dac2a395501f3
Hassan Lakhal <hlakhal-@e1r3p6.1337.ma>
2023-07-15 19:41:29 +0100

    fixed parssing of input info

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,42 +6,41 @@
 /*   By: hlakhal- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 00:26:00 by hlakhal-          #+#    #+#             */
-/*   Updated: 2023/07/15 03:52:05 by hlakhal-         ###   ########.fr       */
+/*   Updated: 2023/07/15 19:40:35 by hlakhal-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
-void dispaly(std::string str)
+void	dispaly(std::string str)
 {
-	if(str.length() > 10)
-		std::cout << std::setw(9) << str.substr(0,9) << ".|";
+	if (str.length() > 10)
+		std::cout << std::setw(9) << str.substr(0, 9) << ".|";
 	else
 		std::cout << std::setw(10) << str << "|";
 }
 
-void	Contact::display_info(void)
+void Contact::display_info(void)
 {
 	dispaly(this->first_name);
 	dispaly(this->last_name);
 	dispaly(this->nick_name);
-	std::cout << std::endl <<"---------------------------------------------" << std::endl;
 }
 
-void	PhoneBook::display()
+void PhoneBook::display()
 {
-	int i;
+	int	i;
 
 	i = 0;
-	//PhoneBook ob;
-	std::cout << std::endl <<"---------------------------------------------" << std::endl;
+	std::cout << std::endl
+				<< "---------------------------------------------" << std::endl;
 	std::cout << "|                 PhoneBook                 |" << std::endl;
 	std::cout << "---------------------------------------------" << std::endl;
 	std::cout << "|   Index  |First Name|Last Name | Nickname |" << std::endl;
 	std::cout << "---------------------------------------------" << std::endl;
-	while(i <= this->id)
+	while (i <= this->id)
 	{
-		std::cout << "|" <<  std::setw(10)<< i << "|";
+		std::cout << "|" << std::setw(10) << i << "|";
 		this->phone[i].display_info();
 		std::cout << std::endl;
 		i++;
@@ -50,8 +49,9 @@ void	PhoneBook::display()
 
 void PhoneBook::add()
 {
-	static int i;
-	PhoneBook phone_ob;
+	static int	i;
+	PhoneBook	phone_ob;
+
 	this->phone[i % 8].init();
 	this->id = i;
 	if (this->id >= 8)
@@ -59,63 +59,90 @@ void PhoneBook::add()
 	i++;
 }
 
-bool parssing(std::string str)
+bool	parssing_l(std::string str)
 {
-	int i;
+	int	i;
+	int	cont;
 
 	i = 0;
-	while(str.c_str()[i])
+	cont = 0;
+	while (str.c_str()[i])
 	{
-		if(isdigit(str[i]))
-			return true;
+		if (isdigit(str[i]))
+			cont++;
 		i++;
 	}
-	return false;
+	if (cont == str.length())
+		return (true);
+	return (false);
+}
+
+bool	parssing(std::string str)
+{
+	int	i;
+
+	i = 0;
+	while (str.c_str()[i])
+	{
+		if (isdigit(str[i]))
+			return (true);
+		i++;
+	}
+	return (false);
 }
 
 std::string Contact::rempler(std::string str)
 {
+	int	flag;
+
 	std::string number_str;
 	std::string input;
-	std::cout << str;
-	getline(std::cin, input);
-	if(!parssing(input))
-		return input;
-	else
+	flag = 0;
+	while (1)
 	{
-		std::cout << "invalid form for name" << std::endl;
+		std::cout << str;
+		getline(std::cin, input);
+		if (!str.compare("Your number phone : "))
+		{
+			if (parssing_l(input))
+				return (input);
+			else
+			{
+				std::cout << "Invalid form for name" << std::endl;
+				flag = 1;
+			}
+		}
+		if (!parssing(input) && flag != 1)
+			return (input);
+		else if (flag == 0)
+			std::cout << "Invalid form for name" << std::endl;
 	}
-
-	// if(!strcmp(str.c_str(),"your number phone : "))
-	// 	this->number = std::atoi(number_str.c_str());
-	return ;
+	return (input.substr(0, 0));
 }
 
 void Contact::init(void)
 {
-	this->first_name = rempler("your first name : ");
-	this->last_name = rempler("your last name : ");
-	this->nick_name = rempler("your Nickname : ");
-	this->number_of_phone = rempler("your number phone : ");
-	this->darkest_secret = rempler("your darkest secret : ");
+	this->first_name = rempler("Your first name : ");
+	this->last_name = rempler("Your last name : ");
+	this->nick_name = rempler("Your Nickname : ");
+	this->number_of_phone = rempler("Your number phone : ");
+	this->darkest_secret = rempler("Your darkest secret : ");
 }
 
 int	main(void)
 {
 	PhoneBook	contact;
+
 	std::string str;
 	while (1)
 	{
 		std::cout << "Enter command ";
 		getline(std::cin, str);
-		if(!strcmp(str.c_str(),"ADD"))
+		if (!strcmp(str.c_str(), "ADD"))
 			contact.add();
-		else if(!strcmp(str.c_str(),"SEARCH"))
+		else if (!strcmp(str.c_str(), "SEARCH"))
 			contact.display();
-		else if(!strcmp(str.c_str(),"EXIT"))
+		else if (!strcmp(str.c_str(), "EXIT"))
 			exit(0);
-		//std::cout << "Hello " << str << "\n";
-		// if(!str)
-		// 	break ;
 	}
 }
